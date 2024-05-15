5e7de95b048cefcd3ba8d80bf64b470fec76a3cb
hhllhhyyds <hhllhhyyds@sina.com>
2024-05-15 16:05:21 +0800

    update python build script

--- build.py
+++ build.py
@@ -18,38 +18,38 @@ def cuda_toolkit_path(build_tool_path):
     path = build_tool_path["cuda_toolkit"][0]
     for p in build_tool_path["cuda_toolkit"][1:]:
         path = os.path.join(path, p)
-    return '"' + path + '"'
+    return path
 
 
-def assemble_cmake_cmd(src_dir, build_tool_path, cmake_options):
-    cmd = cmake_exe_path(build_tool_path=build_tool_path)
-    cmd += " "
-    cmd += src_dir
-    for key, value in cmake_options.items():
-        cmd += " " + "-D" + key + "=" + str(value)
-    return cmd
-
-
-def cmake(build_dir, build_tool_path, cmake_options):
+def cmake(src_dir, build_dir, cmake_exe_path, cmake_options):
     if not os.path.exists(build_dir):
-        os.mkdir(build_dir)
+        os.mkdir(build_dir, mode=777)
     os.chdir(build_dir)
-    src_dir = os.path.dirname(__file__)
-    cmd = assemble_cmake_cmd(
-        src_dir=src_dir, build_tool_path=build_tool_path, cmake_options=cmake_options
-    )
-    subprocess.run(cmd)
-    os.chdir(src_dir)
+    cmd = [cmake_exe_path, src_dir]
+    for key, value in cmake_options.items():
+        cmd.append("-D" + key + "=" + str(value))
+    process = subprocess.Popen(cmd)
+    process.wait()
 
 
-def build(build_dir):
+def build(build_dir, cmake_exe_path):
     assert os.path.exists(build_dir)
     os.chdir(build_dir)
-    subprocess.run("cmake --build .")
-    os.chdir(os.path.dirname(__file__))
+    process = subprocess.Popen([cmake_exe_path, "--build", "."])
+    process.wait()
 
 
-build_tool_path = {
+def cmake_options(build_tool_path):
+    return {
+        "CMAKE_CUDA_ARCHITECTURES": 61,
+        "CMAKE_C_STANDARD": 99,
+        "CMAKE_CXX_STANDARD": 11,
+        "CMAKE_BUILD_TYPE": "Debug",
+        "CUDAToolkit_ROOT": cuda_toolkit_path(build_tool_path),
+    }
+
+
+build_tool_path_win = {
     "cmake": ["C:\\", "Program Files", "CMake"],
     "cuda_toolkit": [
         "C:\\",
@@ -60,26 +60,40 @@ build_tool_path = {
     ],
 }
 
-cmake_options = {
-    "CMAKE_CUDA_ARCHITECTURES": 61,
-    "CMAKE_C_STANDARD": 99,
-    "CMAKE_CXX_STANDARD": 11,
-    "CMAKE_BUILD_TYPE": "Debug",
-    "CUDAToolkit_ROOT": cuda_toolkit_path(build_tool_path),
+build_tool_path_linux = {
+    "cmake": ["/udata", "liuxiaonan", "downloads", "cmake-3.29.3-linux-x86_64"],
+    "cuda_toolkit": [
+        "/udata",
+        "pangen",
+        "pangen_build_env2",
+        "cuda-8.0",
+    ],
 }
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-
     parser.add_argument(
         "-d", "--build-dir", default="build", help="cmake and build directory"
     )
-
     args = parser.parse_args()
 
+    if "win" in sys.platform.lower():
+        build_tool_path = build_tool_path_win
+    elif "linux" in sys.platform.lower():
+        build_tool_path = build_tool_path_linux
+    else:
+        raise Exception("Unsupported platform = " + sys.platform)
+
+    src_dir = os.getcwd()
+    build_dir = os.path.abspath(args.build_dir)
+    cmake_exe = cmake_exe_path(build_tool_path=build_tool_path)
+    cmake_args = cmake_options(build_tool_path=build_tool_path)
+
     cmake(
-        build_dir=args.build_dir,
-        build_tool_path=build_tool_path,
-        cmake_options=cmake_options,
+        src_dir=src_dir,
+        build_dir=build_dir,
+        cmake_exe_path=cmake_exe,
+        cmake_options=cmake_args,
     )
-    build(build_dir=args.build_dir)
+    build(build_dir=build_dir, cmake_exe_path=cmake_exe)
